c847309b2edda907bbd850fecf536bb350083815
adreeanchristopher253-ux <adreeanchristopher253@gmail.com>
2026-09-25 16:08:12 +0530

    Add C program for summing two numbers
    Implemented a C program to add two numbers with input, addition, and output functions.

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,26 @@
-//2. Write a C program to add two numbers.
+#include<stdio.h>
+void input(int *a,int *b)
+{
+  
+  printf("enter two numbers");
+  scanf("%d %d",a,b);
+}
+int add(int *a,int *b,int *result)
+{
+  *result=*a+*b;
+  return *result;
+}
+void output(int final_output)
+{
+  printf("the sum of two numbers is:%d",final_output);
+}
+int main()
+{
+  int a,b,result,final_output;
+  input(&a,&b);
+  final_output=add(&a,&b,&result);
+  output(final_output);
+  return 0;
+}
+
 
